8d2ed394601bd79123bce559ca17c4cec41f61d0
HamdanProg <69153645+HamdanProg@users.noreply.github.com>
2020-08-08 11:11:29 +0700

    Add files via upload

---
+++ MTPAv2.1.py
@@ -0,0 +1,160 @@
+def loading():
+	ld=1
+	while ld<=2:
+		print('\x1bc');print('.')
+		time.sleep(0.05)
+		print('\x1bc');print('..')
+		time.sleep(0.05)
+		print('\x1bc');print('...')
+		time.sleep(0.05)
+		print('\x1bc')
+		ld=ld+1
+print('\x1bc')
+import time
+import math
+#ALAT
+def k():
+	print('\nkode :\na. tambah\ns. kali\nl. lanjut\nPilihan anda : ')
+def res():
+	print('\x1bc')
+	print('Uang masuk sejumlah ',masuk)
+def res1():
+	print('\x1bc')
+	print('Total saldo sejumlah ',tosal)
+def res2():
+	print('\x1bc')
+	print('Sisa saldo sejumlah ',sisal)
+#MENU AWAL
+print('ASISTEN MITRA TOKOPEDIA\nby Hamdan26\n\nGMAIL : hamdanduaenamprog@gmail.com')
+time.sleep(1)
+print('\x1bc');print('\n\nSelamat datang di program kami ^_^')
+time.sleep(2)
+mn=1
+while mn>0:
+	print('\x1bc')
+	print('=====================================MENU UTAMA\nAda yang bisa kami bantu?\n\nkode :\n1. Menghitung keuntungan\n2. Menghitung Persentase keuntungan')
+	menu=input('Pilihan anda : ')
+	if menu=='1':
+		#MENU PERTAMA
+		#UANG MASUK
+		loading()
+		awal=1
+		while awal>0:
+			masuk=int(input('MASUKKAN UANG MASUK\n'))
+			k()
+			hit=1
+			while hit>0:
+				pencet=input('')
+				if pencet=='a':
+					masuk1=int(input())
+					masuk=masuk+masuk1
+					res()
+					hit=hit+1
+					k()
+				elif pencet=='s':
+					masuk1=int(input())
+					masuk=masuk*masuk1
+					res()
+					hit=hit+1
+					k()
+				elif pencet=='l':
+					loading()
+					#TOTAL SALDO
+					tosal=int(input('MASUKKAN TOTAL SALDO\n'))
+					k()
+					hit2=1
+					while hit2>0:
+						pencet=input('')
+						if pencet=='a':
+							tosal1=int(input())
+							tosal=tosal+tosal1
+							res1()
+							hit2=hit2+1
+							k()
+						elif pencet=='s':
+							tosal1=int(input())
+							tosal=tosal*tosal1
+							res1()
+							hit2=hit2+1
+							k()
+						elif pencet=='l':
+							loading()
+							#SISA SALDO
+							sisal= int(input('MASUKKAN SISA SALDO\n'))
+							k()
+							hit3=1
+							while hit3>0:
+								pencet=input('')
+								if pencet=='a':
+									sisal1=int(input())
+									sisal=sisal+sisal1
+									res2()
+									hit3=hit3+1
+									k()
+								elif pencet=='s':
+									sisal1=int(input())
+									sisal=sisal*sisal1
+									res2()
+									hit3=hit3+1
+									k()
+								elif pencet=='l':
+									loading()
+									#OPERASI HITUNG
+									print('\x1bc')
+									print('\nJADI,\nUang masuk = ',masuk,'\nTotal saldo = ',tosal,'\nSisa saldo = ',sisal)
+									print('\nMAKA,\nPendapatan = ', masuk,'\nPengeluaran = ', tosal-sisal,'\nUntung = ', masuk-(tosal-sisal),'\n')
+									#AKHIRI/ULANGI SESI
+									aoru=1
+									while aoru>0:
+										aus=input('Akhiri atau ulangi sesi?\nKode : n. Kembali ke menu awal; m. Akhiri')
+										if aus=='n':
+											print('\x1bc')
+											aoru=-1
+										elif aus=='m':
+											quit()
+										else:
+											aoru=aoru+1
+									else:
+										hit3=-1
+								else:
+									res2()
+									k()
+									hit3=hit3+1
+							else:
+								hit2=-1
+						else:
+							res1()
+							k()
+							hit2=hit2+1
+					else:
+						hit=-1
+				else:
+					res()
+					k()
+					hit=hit+1
+			else:
+				awal=-1
+		else:
+			mn=mn+1
+	elif menu=='2':
+		loading()
+		mn=-1
+	else:
+		mn=mn+1
+#MENU KEDUA
+tanyawaktu=int(input('Sudah berapa bulan anda menjadi Mitra Tokopedia ?\nJawab : '))
+var1=float(input('Keuntungan pada bulan pertama : '))
+var2=float(input('Keuntungan pada bulan kemarin : '))
+var3=float(input('Keuntungan pada bulan sekarang : '))
+nilaimx=max(var1,var2,var3)
+var4=((var3/var1))*100-100
+var5=((var3/var2))*100-100
+loading()
+print('MAKA,\nKenaikan untung terhadap bulan pertama : ',var4,' %')
+print('Kenaikan untung terhadap bulan kemarin  : ',var5, ' %')
+sttvar1=round((var1/nilaimx)*50)
+sttvar2=round((var2/nilaimx)*50)
+sttvar3=round((var3/nilaimx)*50)
+print('Untung bulan pertama : \n'+']'*sttvar1,'\n')
+print('Untung bulan kemarin : \n'+']'*sttvar2,'\n')
+print('Untung bulan sekarang : \n'+']'*sttvar3,'\n')
---
+++ MTPAv2.2.py
@@ -0,0 +1,181 @@
+def uak(x,y,z):
+	x=1
+	while x>0:
+		y=input('Kode : n. Kembali ke menu awal; m. Keluar\nPilihan anda : ')
+		if y=='n':
+			print('\x1bc')
+			loading()
+			x=-1
+		elif y=='m':
+			quit()
+		else:
+			print('\x1bc')
+			z()
+			x=x+1
+
+def loading():
+	ld=1
+	while ld<=50:
+		print('\x1bc');print('='*ld);time.sleep(0.004)
+		ld=ld+1
+		print('\x1bc')
+#IMPORT
+print('\x1bc')
+import time
+import math
+#ALAT
+def k():
+	print('\nkode :\na. tambah\ns. kali\nl. lanjut\nPilihan anda : ')
+def res():
+	print('\x1bc')
+	print('Uang masuk sejumlah Rp',masuk)
+def res1():
+	print('\x1bc')
+	print('Total saldo sejumlah Rp',tosal)
+def res2():
+	print('\x1bc')
+	print('Sisa saldo sejumlah Rp',sisal)
+#MENU AWAL
+print('\x1bc');print('S        ###              ###');time.sleep(0.1);print('E      #     #          #     #');time.sleep(0.1);print('L     #       #        #       #');time.sleep(0.1);print('A     #       #        #       #');time.sleep(0.1);print('M');time.sleep(0.1);print('A');time.sleep(0.1);print('T       ');time.sleep(0.1);print('           #################');time.sleep(0.1);print('D          #               #');time.sleep(0.1);print('A           #             #');time.sleep(0.1);print('T             ##       ##');time.sleep(0.1);print('A               #######');time.sleep(0.1);print('N');time.sleep(0.1);print('G')
+time.sleep(1)
+print('\x1bc');print('MITRA TOKOPEDIA ASISTANT\nmetode penghitungan semi otomatis untuk mitra')
+time.sleep(2.5)
+mn=1
+while mn>0:
+	print('\x1bc')
+	print('^<^ : MENU UTAMA\n\nAda yang bisa kami bantu?\n\nkode :\n1. Menghitung keuntungan\n2. Statistik keuntungan\n3. Intruksi penggunaan\n4. Tentang Pembuat\n5. Keluar')
+	menu=input('Pilihan anda : ')
+	if menu=='1':
+#MENU PERTAMA
+		#UANG MASUK
+		loading()
+		awal=1
+		while awal>0:
+			masuk=int(input('MASUKKAN UANG MASUK\nRp'))
+			k()
+			hit=1
+			while hit>0:
+				pencet=input('')
+				if pencet=='a':
+					masuk1=int(input('Rp'))
+					masuk=masuk+masuk1
+					res()
+					hit=hit+1
+					k()
+				elif pencet=='s':
+					masuk1=int(input('Rp'))
+					masuk=masuk*masuk1
+					res()
+					hit=hit+1
+					k()
+				elif pencet=='l':
+					loading()
+					#TOTAL SALDO
+					tosal=int(input('MASUKKAN TOTAL SALDO\nRp'))
+					k()
+					hit2=1
+					while hit2>0:
+						pencet=input('')
+						if pencet=='a':
+							tosal1=int(input('Rp'))
+							tosal=tosal+tosal1
+							res1()
+							hit2=hit2+1
+							k()
+						elif pencet=='s':
+							tosal1=int(input('Rp'))
+							tosal=tosal*tosal1
+							res1()
+							hit2=hit2+1
+							k()
+						elif pencet=='l':
+							loading()
+							#SISA SALDO
+							sisal= int(input('MASUKKAN SISA SALDO\nRp'))
+							k()
+							hit3=1
+							while hit3>0:
+								pencet=input('')
+								if pencet=='a':
+									sisal1=int(input('Rp'))
+									sisal=sisal+sisal1
+									res2()
+									hit3=hit3+1
+									k()
+								elif pencet=='s':
+									sisal1=int(input('Rp'))
+									sisal=sisal*sisal1
+									res2()
+									hit3=hit3+1
+									k()
+								elif pencet=='l':
+									loading()
+									#OPERASI HITUNG
+									def oph():
+										print('\x1bc')
+										print('\nJADI,\nUang masuk = Rp',masuk,'\nTotal saldo = Rp',tosal,'\nSisa saldo = Rp',sisal)
+										print('\nMAKA,\nPendapatan = Rp', masuk,'\nPengeluaran = Rp', tosal-sisal,'\nUntung = Rp', masuk-(tosal-sisal),'\n\n*Silahkan catat di buku catatan\n')
+									oph()
+									#AKHIRI/ULANGI SESI
+									uak(1,2,oph)
+									hit3=-1
+								else:
+									res2()
+									k()
+									hit3=hit3+1
+							else:
+								hit2=-1
+						else:
+							res1()
+							k()
+							hit2=hit2+1
+					else:
+						hit=-1
+				else:
+					res()
+					k()
+					hit=hit+1
+			else:
+				awal=-1
+		else:
+			mn=mn+1
+	elif menu=='2':
+		loading()
+#MENU KEDUA
+		var1=float(input('Keuntungan pada bulan pertama : Rp'))
+		var2=float(input('Keuntungan pada bulan kemarin : Rp'))
+		var3=float(input('Keuntungan pada bulan sekarang : Rp'))
+		nilaimx=max(var1,var2,var3)
+		var4=((var3/var1))*100-100
+		var5=((var3/var2))*100-100
+		loading()
+		def aou2():
+			print('MAKA,\nKenaikan untung terhadap bulan pertama : ',var4,' %')
+			print('Kenaikan untung terhadap bulan kemarin : ',var5, ' %')
+			sttvar1=round((var1/nilaimx)*50)
+			sttvar2=round((var2/nilaimx)*50)
+			sttvar3=round((var3/nilaimx)*50)
+			print('Untung bulan pertama : \n'+']'*sttvar1,'\n')
+			print('Untung bulan kemarin : \n'+']'*sttvar2,'\n')
+			print('Untung bulan sekarang : \n'+']'*sttvar3,'\n')
+			print('\n*Silahkan catat di buku catatan\n')
+		aou2()
+		#AKHIRI/ULANGI SESI
+		uak(3,4,aou2)
+		mn=mn+1
+#MENU KETIGA
+	elif menu=='3':
+		loading()
+		def aou3():
+			print('^<^ : INTRUKSI PENGGUNAAN\n');print('Hanya masukkan angka pada saat memasukkan nilai uang\nJika dilanggar : akan muncul EROR\n\nHanya masukkan kode yang ada di dalam list kode\nJika dilanggar : perintah tidak akan tersampaikan')
+		aou3()
+		#AKHIRI/ULANGI SESI
+		print('\n'*6)
+		uak(5,6,aou3)
+		mn=mn+1
+#MENU KELIMA
+	elif menu=='5':
+		quit()
+		
+	else:
+		mn=mn+1
